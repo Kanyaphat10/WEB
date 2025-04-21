7a616a0f03e636e83dffdaab2d3d22aac62f896e
Kanyaphat Chaithawip <140889485+Kanyaphat10@users.noreply.github.com>
2025-04-21 23:58:34 +0700

    landing page

---
+++ admin_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>admin page</title>
+
+        <!-- custom css file link -->
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body class="admin-page">
+        <div class="container">
+            <div class="content">
+                <!-- <h3>hi, <span>Admin</span></h3> -->
+                <h1>WELCOME</h1>
+                <!-- <p>this is an admin page</p>-->
+                <p class="des">Greetings Lady and Gentleman to World of Movie</p>
+                <a href="sign_in.php" class="btn">Sign In</a>
+                <!-- <a href="register_form.php" class="btn">register</a>
+                <a href="logout.php" class="btn">logout</a> -->
+                <p class="don">Don’t have an account? <a href="sign_in.php" class="btn-bold">Sign Up</a></p>
+            </div>
+        </div>
+    </body>
+</html>
